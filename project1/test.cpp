2e7c4e773bb5baeee2d3cae136987c365e1ba183
JubeatWwW <l6104400@gmail.com>
2016-10-18 22:59:25 +0800

    fix grammar to fit spec

---
+++ project1/test.cpp
@@ -0,0 +1,25 @@
+//print Hello World
+
+void main(){
+    int a;
+    double b;
+    print "Hello World";
+    print "aa""\"bb\\cc\"\\"
+    a = 1+1;
+    b = 1.23;
+    if(a > 1){
+        b = b*1.23e-1;
+    }
+    if(a != 1 || b >= 2){
+        b = b*1.23e-1;
+    }
+    /*
+        RRRRR
+        /* /*  */ a = 1 * 87;
+    /* a a a*/
+    /*
+     *
+     * test
+     *
+     * */ b = 1 + 2 + 3;
+}
